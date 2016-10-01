e5e904f299ee7e6aa6f8604c630d78e2372c6bb3
pineal <pineal@outlook.com>
2016-09-30 23:42:22 -0700

    Dijkstra algorithm C++ implement

---
+++ Algorithms/Dijkstra/Dijkstra_Adjacent_List.cpp
@@ -0,0 +1,73 @@
+//Reference:
+//http://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-using-priority_queue-stl/
+//Time Complexity: O(ElogV)
+#include <vector>
+#include <priority_queue>
+using namespace std;
+class Graph {
+    int V;
+    vector<vector<pair<int, int>>> adj;
+public:
+    Graph(int V);
+    void addEdge(int u, int v, int w);
+    void dijkstra(int s);
+};
+
+Graph::Graph(int V) {
+    this -> V = V;
+    adj = vector<vector<pair<int, int>>>(V);
+}
+
+void Graph::addEdge(int u, int v, int w){
+    adj[u].emplace_back(v, w);
+    adj[v].emplace_back(u, w);
+}
+
+void Graph::dijkstra(int src) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+    vector<int> dist (V, INT_MAX);
+    min_heap.emplace(0, src);
+    dist[src] = 0;
+    while (!min_heap.empty()) {
+        int u = min_heap.top().second;
+        min_heap.pop();
+        for (auto iter = adj[u].begin(); iter != adj[u].end(); iter++) {
+            int v = iter->first;
+            int w = iter->second;
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                min_heap.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < V; ++i) {
+        printf("%d \t\t %d\n", i, dist[i]);
+    }
+}
+
+int main(int c, char** v) {
+    // create the graph given in above fugure
+    int V = 9;
+    Graph g(V);
+    
+    //  making above shown graph
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+    g.dijkstra(0);
+    
+    return 0;
+}
---
+++ Algorithms/Dijkstra/Dijkstra_Adjacent_Matrix.cpp
@@ -0,0 +1,55 @@
+//http://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/
+//Time Complexity O(V^2)
+#include <vector>
+#include <deque>
+using namespace std;
+#define V 9
+int minDistance(const vector<int> & dist, const deque<bool> & visited) {
+    int min = INT_MAX;
+    int min_index = 0;
+    for (int i = 0; i < V; i++) {
+        if (visited[i] == false && dist[i] < min) {
+            min = dist[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+void dijkstra(const vector<vector<int>> & graph, int src) {
+    vector<int> dist(V, INT_MAX);
+    deque<bool> visited(V, false);
+    dist[src] = 0;
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, visited);
+        visited[u] = true;
+        for (int v = 0; v < V; v++) {
+            if (!visited[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < V; i++) {
+        printf("%d \t\t %d\n", i, dist[i]);
+    }
+    
+}
+
+
+
+int main(int c, char** v) {
+    vector<vector<int>> graph = {
+        {0, 4, 0, 0, 0, 0, 0, 8, 0},
+        {4, 0, 8, 0, 0, 0, 0, 11, 0},
+        {0, 8, 0, 7, 0, 4, 0, 0, 2},
+        {0, 0, 7, 0, 9, 14, 0, 0, 0},
+        {0, 0, 0, 9, 0, 10, 0, 0, 0},
+        {0, 0, 4, 14, 10, 0, 2, 0, 0},
+        {0, 0, 0, 0, 0, 2, 0, 1, 6},
+        {8, 11, 0, 0, 0, 0, 1, 0, 7},
+        {0, 0, 2, 0, 0, 0, 6, 7, 0}
+    };
+    dijkstra(graph, 0);
+    return 0;
+}
